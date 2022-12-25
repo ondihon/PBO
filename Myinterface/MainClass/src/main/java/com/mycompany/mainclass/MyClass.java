6cb31e69a6152bd7b5beb56bafef659dfd9e52f3
ondihon <89757342+ondihon@users.noreply.github.com>
2022-12-25 23:26:31 +0700

    Add files via upload

---
+++ Myinterface/MainClass/src/main/java/com/mycompany/mainclass/MyClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mainclass;
+
+/**
+ *
+ * @author asus
+ */
+public class MyClass implements Myinterface {
+    @Override
+    public void myMethod1() {
+        System.out.println("Hello");
+    }
+ 
+    @Override
+    public void myMethod2() {
+        System.out.println("Hello Juga");
+    }
+}
